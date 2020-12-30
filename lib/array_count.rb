ed864c8fbbeabdfb1052d898ded5eaa48b9645dd
Ziad Zebib <zebibziad@gmail.com>
2020-12-30 20:25:31 +0000

    Automatically backed up by Learn

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,7 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
 array.count do |num|
-
+  
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
 end
